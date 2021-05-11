dddf779ae84f1fc2a219cd2162f23b29d5936ef2
Balbader <15971200+Balbader@users.noreply.github.com>
2021-05-11 14:16:01 +0200

    update

--- src/index.js
+++ src/index.js
@@ -28,13 +28,17 @@ class Board extends React.Component {
     super(props);
     this.state = {
       squares: Array(9).fill(null),
+      xIsNext: true, //will flip at each turn to determine which player goes next
     };
   }
 
   handleClick(i) {
     const squares = this.state.squares.slice(); // use slice() to create a copy of the squares array to modify instead of modifying the existing one.
-    squares[i] = 'X';
-    this.setState({squares: squares});
+    squares[i] = this.state.xIsNext ? 'X' : 'O'; // changing input based on xIsNext value
+    this.setState({
+      squares: squares,
+      xIsNext: !this.state.xIsNext,
+    });
   }
 
   renderSquare(i) {
@@ -47,7 +51,7 @@ class Board extends React.Component {
   }
 
   render() {
-    const status = 'Next player: X';
+    const status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O'); // Displays which player has the next turn on top of page
 
     return (
       <div>
@@ -86,6 +90,26 @@ class Game extends React.Component {
       </div>
     );
   }
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
 
 // ========================================
